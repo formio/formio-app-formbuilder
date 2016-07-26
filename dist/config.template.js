913bafabbe5ca39dfb48338ea888b408c4ebb64f
Travis Tidwell <travis@form.io>
2016-07-26 16:37:46 -0500

    Updating the template url.

--- dist/config.template.js
+++ dist/config.template.js
@@ -1,18 +1,30 @@
-angular.module('formioApp').constant('AppConfig', {
 {% if domain %}
-  appUrl: '{{ domain }}',
-  apiUrl: '{{ domain }}',
+var APP_URL = '{{ domain }}';
+var API_URL = '{{ domain }}';
 {% else %}
-  appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
-  apiUrl: '{{ protocol }}://api.{{ host }}',
+var APP_URL = '{{ protocol }}://{{ path }}.{{ host }}';
+var API_URL = '{{ protocol }}://api.{{ host }}';
 {% endif %}
+
+// Parse query string
+var query = {};
+location.search.substr(1).split("&").forEach(function(item) {
+  query[item.split("=")[0]] = item.split("=")[1] && decodeURIComponent(item.split("=")[1]);
+});
+
+APP_URL = query.appUrl || APP_URL;
+API_URL = query.apiUrl || API_URL;
+
+angular.module('formioApp').constant('AppConfig', {
+  appUrl: APP_URL,
+  apiUrl: API_URL,
   forms: {
     userLoginForm: APP_URL + '/user/login'
   },
   resources: {
     user: {
-      form: APP_URL + '/user',
-        resource: 'UserResource'
+      form: APP_URL + '/admin',
+      resource: 'UserResource'
     }
   }
 });
