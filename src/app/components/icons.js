f4c3919ad0ec3d4d937bd62a8a6b3031133e0f21
Randall Knutson <randall@form.io>
2016-08-02 15:57:13 -0500

    Add custom "icons" component.

---
+++ src/app/components/icons.js
@@ -0,0 +1,96 @@
+(function() {
+  'use strict';
+  angular
+    .module('formioApp')
+    .config([
+      'formioComponentsProvider',
+      function (formioComponentsProvider) {
+        formioComponentsProvider.register('icons', {
+          title: 'Icons',
+          template: 'formio/components/icons.html',
+          controller: ['$scope', function($scope) {
+            $scope.setValue = function(value) {
+              $scope.data[$scope.component.key] = value;
+            };
+          }],
+          group: 'custom',
+          icon: 'fa fa-smile-o',
+          settings: {
+            icons: [
+              {
+                value: 5,
+                icon: 'fa fa-thumbs-o-up'
+              },
+              {
+                value: 4,
+                icon: 'fa fa-smile-o'
+              },
+              {
+                value: 3,
+                icon: 'fa fa-meh-o'
+              },
+              {
+                value: 2,
+                icon: 'fa fa-frown-o'
+              },
+              {
+                value: 1,
+                icon: 'fa fa-thumbs-o-down'
+              },
+            ],
+            fontSize: '2em',
+            activeColor: 'green',
+            multiple: false,
+            protected: false,
+            persistent: true,
+            tableView: false
+          },
+          views: [
+            {
+              name: 'Display',
+              template: 'formio/components/icons/display.html'
+            },
+            {
+              name: 'Validation',
+              template: 'formio/components/signature/validate.html'
+            },
+            {
+              name: 'API',
+              template: 'formio/components/common/api.html'
+            },
+            {
+              name: 'Layout',
+              template: 'formio/components/common/layout.html'
+            },
+            {
+              name: 'Conditional',
+              template: 'formio/components/common/conditional.html'
+            }
+          ]
+        });
+      }
+    ])
+    .run([
+      '$templateCache',
+      'FormioUtils',
+      function ($templateCache, FormioUtils) {
+        $templateCache.put('formio/components/icons.html', FormioUtils.fieldWrap('<i ng-repeat="icon in component.icons" ng-style="{ color: (data[component.key] == icon.value ? component.activeColor : \'\'), \'font-size\': component.fontSize }" ng-click="setValue(icon.value)" class="{{icon.icon}}" aria-hidden="true"></i>'));
+        $templateCache.put('formio/components/icons/display.html', '' +
+          '<ng-form>' +
+          '  <form-builder-option property="label"></form-builder-option>' +
+          '  <value-builder data="component.icons" label-label="Icon" label-property="icon"></value-builder>' +
+          '  <form-builder-option property="fontSize" label="Font Size"></form-builder-option>' +
+          '  <form-builder-option property="activeColor" label="Active Color"></form-builder-option>' +
+          '  <form-builder-option property="customClass"></form-builder-option>' +
+          '  <form-builder-option property="protected"></form-builder-option>' +
+          '  <form-builder-option property="persistent"></form-builder-option>' +
+          '  <form-builder-option property="tableView"></form-builder-option>' +
+          '</ng-form>');
+        $templateCache.put('formio/components/icons/validate.html',
+          '<ng-form>' +
+          '<form-builder-option property="validate.required"></form-builder-option>' +
+          '</ng-form>'
+        );
+      }
+    ]);
+})();
--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,16 +6,21 @@
       'AppConfig',
       'FormioProvider',
       'FormioAuthProvider',
+      'formioComponentsProvider',
       function(
         AppConfig,
         FormioProvider,
-        FormioAuthProvider
+        FormioAuthProvider,
+        formioComponentsProvider
       ) {
         FormioProvider.setAppUrl(AppConfig.appUrl);
         FormioProvider.setBaseUrl(AppConfig.apiUrl);
         FormioAuthProvider.setForceAuth(true);
         FormioAuthProvider.setStates('auth.login', 'home');
         FormioAuthProvider.register('login', 'user', 'login');
+
+        // Add a new group to formioComponentsProvider.
+        formioComponentsProvider.addGroup('custom', {title: 'Custom Components'});
       }
     ]);
 })();
