9ce082ff58ea45527ba062fc2dfce56d58ddafa6
Travis Tidwell <travis@form.io>
2017-05-10 20:32:15 -0500

    Updates to the form builder app.

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,15 @@
 'use strict';
 
 var gulp = require('gulp');
-var wrench = require('wrench');
-
-/**
- *  This will load all js or coffee files in the gulp directory
- *  in order to load all gulp tasks
- */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
-
+require('./gulp/build');
+require('./gulp/conf');
+require('./gulp/e2e-tests');
+require('./gulp/inject');
+require('./gulp/scripts');
+require('./gulp/server');
+require('./gulp/styles');
+require('./gulp/unit-tests');
+require('./gulp/watch');
 
 /**
  *  Default task clean temporaries directories and launch the
